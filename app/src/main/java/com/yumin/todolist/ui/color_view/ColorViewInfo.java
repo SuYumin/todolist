c895261c290316c94f24a065861f35b736e5d43d
stellaymusu <stella7929@hotmail.com>
2021-05-21 00:30:54 +0800

    add customize color view and choose color dialog for list info

---
+++ app/src/main/java/com/yumin/todolist/ui/color_view/ColorViewInfo.java
@@ -0,0 +1,11 @@
+package com.yumin.todolist.ui.color_view;
+
+public class ColorViewInfo {
+    boolean isSelect;
+    int colorValue;
+
+    public ColorViewInfo(boolean isSelect, int colorValue){
+        this.isSelect = isSelect;
+        this.colorValue = colorValue;
+    }
+}
